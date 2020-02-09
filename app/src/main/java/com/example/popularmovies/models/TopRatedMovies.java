8418f618d8ee9c4e500f18481b046a315fdcbf5c
bazrafkan <bazrafkan.mb@gmail.com>
2020-02-09 15:47:35 +0100

    feat: add top rated movies model

---
+++ app/src/main/java/com/example/popularmovies/models/TopRatedMovies.java
@@ -0,0 +1,46 @@
+package com.example.popularmovies.models;
+
+import android.net.Uri;
+
+import com.example.popularmovies.BuildConfig;
+import com.example.popularmovies.utils.NetworkUtils;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TopRatedMovies {
+    private final static String TMDB_URL =
+            "https://api.themoviedb.org/3/movie/top_rated";
+    private final static String PARAM_API_KEY = "api_key";
+
+    @SerializedName("results")
+    List<ListMovies> listMovies = null;
+
+    public static List<ListMovies> getTopRatedMovies() {
+        Map<String, String> params = new HashMap<String, String>();
+        try {
+            params.put(PARAM_API_KEY, BuildConfig.tmdb_api_key);
+            URL url = NetworkUtils.buildUrl(TMDB_URL, params);
+            String result = NetworkUtils.getResponseFromHttpUrl(url);
+            return parseJSON(result).listMovies;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static TopRatedMovies parseJSON(String response) {
+        Gson gson = new GsonBuilder().create();
+        TopRatedMovies topRatedMoviesResponse = gson.fromJson(response, TopRatedMovies.class);
+        return topRatedMoviesResponse;
+    }
+
+}
