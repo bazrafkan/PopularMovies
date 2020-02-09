236a3e1575a61625130c4f5b41dcf635e735565d
bazrafkan <bazrafkan.mb@gmail.com>
2020-02-09 12:48:59 +0100

    feat: add network utility

---
+++ app/src/main/java/com/example/popularmovies/utils/NetworkUtils.java
@@ -0,0 +1,48 @@
+package com.example.popularmovies.utils;
+
+import android.net.Uri;
+
+import com.example.popularmovies.BuildConfig;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+import java.util.Scanner;
+
+public class NetworkUtils {
+
+    public static URL buildUrl(String urlPath, Map<String, String> params) {
+        Uri.Builder builder = Uri.parse(urlPath).buildUpon();
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            builder.appendQueryParameter(entry.getKey(), entry.getValue());
+        }
+        Uri builtUri = builder.build();
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
