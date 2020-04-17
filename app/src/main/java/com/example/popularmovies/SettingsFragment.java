c9d97b7d07fc911a0d00cf10c3cb04f2f76d3496
bazrafkan <bazrafkan.mb@gmail.com>
2020-04-17 14:29:50 +0200

    fix: add summary to list preference

--- app/src/main/java/com/example/popularmovies/SettingsFragment.java
+++ app/src/main/java/com/example/popularmovies/SettingsFragment.java
@@ -1,12 +1,61 @@
 package com.example.popularmovies;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.pref_visuakizer);
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+        int count = preferenceScreen.getPreferenceCount();
+        for (int i = 0; i < count; i++) {
+            Preference preference = preferenceScreen.getPreference(i);
+            if (preference instanceof ListPreference) {
+                String value = sharedPreferences.getString(preference.getKey(), "");
+                setListPreferenceSummary((ListPreference) preference, value);
+            }
+        }
+    }
+
+    private void setListPreferenceSummary(ListPreference listPreference, String value) {
+        int index = listPreference.findIndexOfValue(value);
+        if (index >= 0) {
+            listPreference.setSummary(listPreference.getEntries()[index]);
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference preference = findPreference(key);
+        if (preference != null) {
+            if (preference instanceof ListPreference) {
+                String value = sharedPreferences.getString(preference.getKey(), "");
+                setListPreferenceSummary((ListPreference) preference, value);
+            }
+        }
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getPreferenceScreen()
+                .getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        getPreferenceScreen()
+                .getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
     }
 }
