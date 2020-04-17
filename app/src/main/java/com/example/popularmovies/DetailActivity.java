bb78cd524016b2765992f5b21e4962e65f43d4f7
bazrafkan <bazrafkan.mb@gmail.com>
2020-04-17 10:25:30 +0200

    feat: add settings activity

--- app/src/main/java/com/example/popularmovies/DetailActivity.java
+++ app/src/main/java/com/example/popularmovies/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.example.popularmovies;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +11,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -51,7 +54,6 @@ public class DetailActivity extends AppCompatActivity implements MoviesTask.Asyn
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        setTitle("MovieDetail");
         mLoadingProgressBar = findViewById(R.id.pb_loading_indicator);
         mTitleTextView = findViewById(R.id.tv_title_movie);
         mPosterImageView = findViewById(R.id.iv_image_detail);
@@ -73,6 +75,23 @@ public class DetailActivity extends AppCompatActivity implements MoviesTask.Asyn
             listReviewsTask.execute(id);
             moviesTask.execute(id);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
